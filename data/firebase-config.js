3c56cd52455380eff3c1be1e3175a9f6ffac9c85
lilyanparma <lilyanparma@gmail.com>
2025-06-11 03:28:18 +0100

    Update firebase-config.js

--- data/firebase-config.js
+++ data/firebase-config.js
@@ -7,11 +7,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyDRtJBPOjpfXXfsaDo98E_NTT7KWzz8gj4",
   authDomain: "pharmacy-system-9def6.firebaseapp.com",
-  databaseURL: "https://pharmacy-system-9def6-default-rtdb.firebaseio.com", //
-  projectId: "pharmacy-system-9def6", //
-  storageBucket: "pharmacy-system-9def6.appspot.com", //
-  messagingSenderId: "630855008414", //
-  appId: "1:630855008414:web:b464bcb81c8a2091d0fb8f", //
+  databaseURL: "https://pharmacy-system-9def6-default-rtdb.firebaseio.com",
+  projectId: "pharmacy-system-9def6",
+  storageBucket: "pharmacy-system-9def6.firebasestorage.app",
+  messagingSenderId: "630855008414",
+  appId: "1:630855008414:web:b464bcb81c8a2091d0fb8f",
+  measurementId: "G-LC5NDKP6QZ"
 };
 
 // ثانياً: تهيئة تطبيق Firebase
